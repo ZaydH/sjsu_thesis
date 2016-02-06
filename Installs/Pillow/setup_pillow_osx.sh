690d5b1ee35af049cd2af3e55e9cbb96fac5f2f8
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2016-02-06 05:31:31 -0800

    Committing the script to install pillow on OSX.  Verified on Yosemite.

---
+++ Installs/Pillow/setup_pillow_osx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# As good practice always update brew first.
+brew update 
+
+# Install Pillow's dependencies
+pip install --upgrade pip
+brew install libjpeg
+brew install libtiff
+brew install webp
+brew install little-cms2
+
+# Install Pillow
+pip install Pillow --upgrade
+
+# For more information, on install Pillow, see below:
+# https://pillow.readthedocs.org/en/latest/installation.html
