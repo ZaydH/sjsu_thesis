e0beab2d7af50c8eba3005e3b7eec6d19bc3b77c
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-02-06 20:47:55 -0800

    Fixing errors in the puzzle_piece class as a whole as well as well as in the unit tester for the rotate functions.

--- Puzzle_Generator/puzzle_generator/puzzle_piece.py
+++ Puzzle_Generator/puzzle_generator/puzzle_piece.py
@@ -9,7 +9,13 @@ class PuzzlePiece:
         degree_90 = 90
         degree_180 = 180
         degree_270 = 270
-        _degree_360 = 360 # Internal use only
+        _degree_360 = 360  # Internal use only
+
+        @staticmethod
+        def get_all_rotations():
+            return [PuzzlePiece.Rotation.degree_0, PuzzlePiece.Rotation.degree_90,
+                    PuzzlePiece.Rotation.degree_180, PuzzlePiece.Rotation.degree_270]
+
 
     def __init__(self, width):
         # Number of pixels in the width of the piece
@@ -17,6 +23,7 @@ class PuzzlePiece:
         # Create the matrix for storing the pixel information
         self._pixels = [ [ -1 for x in range(0, width)] for x in range(0,width)]
         # Set a random rotation
+        self._rotation = -1 # Create a reference to prevent compiler warnings
         self.set_rotation(PuzzlePiece.Rotation.degree_0)
 
     def _get_unrotated_coordinates(self, rotated_x, rotated_y):
@@ -25,44 +32,60 @@ class PuzzlePiece:
         rotated x/y coordinates, this function calculates unrotated x/y coordinates.
 
         This function is needed since the puzzle piece's pixels are stored unrotated.
-        Hence, we need
-        :param x:
-        :param y:
+        Hence, we need to rotate rotated x/y coordinates into their unrotated
+        equivalents.
+
+        :param rotated_x: An already rotated x coordinate
+        :param rotated_y: An already rotated y coordinate
+
         :return: Tuple in the format (unrotated_x, unrotated_y)
         """
         # Number of rotations is equal to the number of rotations required to make the
         # rotated x/y values full circle.  Hence, 0degree rotation takes no 90 degrees.
         # 90degree rotation takes 3 rotations, 180degrees takes 2, and 270 takes 1.
-        numb_90_degree_rotations = (PuzzlePiece.Rotation._degree_360 - self._rotation) % PuzzlePiece.Rotation._degree_360
-        numb_90_degree_rotations /= PuzzlePiece.Rotation._degree_90
+        # noinspection PyProtectedMember,PyUnresolvedReferences
+        numb_90_degree_rotations = (PuzzlePiece.Rotation._degree_360.value - self._rotation.value) \
+                                   % PuzzlePiece.Rotation._degree_360.value
+        numb_90_degree_rotations /= PuzzlePiece.Rotation.degree_90.value
+        # Handle case where loop is not run.
+        (unrotated_x, unrotated_y) = (rotated_x, rotated_y)
         for i in range(0, numb_90_degree_rotations):
-            (unrotated_x, unrotated_y) = self._get_rotated_coordinates(rotated_x, rotated_y)
+            (unrotated_x, unrotated_y) = self._get_rotated_coordinates(rotated_x,
+                                                                       rotated_y,
+                                                                       PuzzlePiece.Rotation.degree_90 )
             # Updated rotated values in case need to rotated again
             (rotated_x, rotated_y) = (unrotated_x, unrotated_y)
         # Return the unrotated coordinates
-        return (unrotated_x, unrotated_y)
+        return unrotated_x, unrotated_y
 
-    def _get_rotated_coordinates(self, unrotated_x, unrotated_y):
+    def _get_rotated_coordinates(self, unrotated_x, unrotated_y, rotation=-1):
         """
         Given a specified puzzle piece with its own rotation and width,
         this function calculates rotated x and y values.  This keeps the
         object data unchanged during a rotation.
 
-        :param x:   x coodinate to rotate
-        :param y:   y coordinate to rotate
+        :param unrotated_x:   x coordinate to rotate
+        :param unrotated_y:   y coordinate to rotate
+        :param rotation:      Number of degrees to rotate.  Uses Rotation enum class.  If
+                              no rotation is specied, uses the specified object's rotation.
         :return:    Tuple in format (rotated_x, rotated_y)
         """
-        numb_90_degree_rotations = self._rotation / PuzzlePiece.Rotation.degree_90
+        # If no rotation i specified then use the specified object's rotation
+        if rotation == -1:
+            rotation = self._rotation
+        # Calculate number of 90 degree rotations to perform.
+        numb_90_degree_rotations = rotation.value / PuzzlePiece.Rotation.degree_90.value
         # Each iteration of the loop rotates the x, y coordinates 90 degrees
         # Each a 180 degree rotation is two 90 degree rotations
+        (rotated_x, rotated_y) = (unrotated_x, unrotated_y)  # In case loop is never run
         for i in range(0, numb_90_degree_rotations):
-            # Calculte the rotated x and y
+            # Calculate the rotated x and y
             rotated_x = self.get_width() - 1 - unrotated_y
             rotated_y = unrotated_x
             # update the unrotated x/y values in case need to rotate again
             (unrotated_x, unrotated_y) = (rotated_x, rotated_y)
         # Return the rotated x and y coordinates
-        return (rotated_x, rotated_y)
+        return rotated_x, rotated_y
 
 
     def setpixel(self, x, y, pixel):
@@ -73,14 +96,14 @@ class PuzzlePiece:
         :param y:       Pixel's y coordinate
         :param pixel:   New pixel value.  A Tuple in the format (Red, Green, Blue)
         """
-        if(x >= self._width):
+        if x >= self._width:
             raise ValueError("Pixel's \"x\" coordinate must be between 0 and width - 1")
-        if(y >= self._width):
+        if y >= self._width:
             raise ValueError("Pixel's \"y\" coordinate must be between 0 and width - 1")
         # Correct for any rotation
         (unrotated_x, unrotated_y) = self._get_unrotated_coordinates(x, y)
         # Updated the pixel value using the unrotated x and y coordinates
-        self._pixels[unrotated_x, unrotated_y] = pixel
+        self._pixels[unrotated_x][unrotated_y] = pixel
 
     def getpixel(self, x, y, pixel):
         """
@@ -90,9 +113,9 @@ class PuzzlePiece:
         :param y:       Pixel's y coordinate
         :param pixel:   New pixel value.  A Tuple in the format (Red, Green, Blue)
         """
-        if(x >= self._width):
+        if x >= self._width:
             raise ValueError("Pixel's \"x\" coordinate must be between 0 and width - 1")
-        if(y >= self._width):
+        if y >= self._width:
             raise ValueError("Pixel's \"y\" coordinate must be between 0 and width - 1")
         self._pixels = pixel
 
@@ -103,7 +126,7 @@ class PuzzlePiece:
         :param rotation: Type Rotation.  The rotation of this
                          puzzle piece.
         """
-        if(rotation % self.Rotation.degree_90 != 0):
+        if rotation.value % self.Rotation.degree_90.value != 0:
             raise ValueError("Invalid rotation value.")
         self._rotation = rotation
 
--- Puzzle_Generator/puzzle_generator/test_puzzle_piece.py
+++ Puzzle_Generator/puzzle_generator/test_puzzle_piece.py
@@ -3,6 +3,8 @@ from puzzle_piece import PuzzlePiece
 
 class PuzzlePieceTestCase(unittest.TestCase):
 
+    PRINT_DEBUG_MESSAGES = False
+
     def test_get_rotated_puzzle_piece(self):
         """
         Checks whether rotation of a puzzle piece works correctly
@@ -11,11 +13,63 @@ class PuzzlePieceTestCase(unittest.TestCase):
         piece = PuzzlePiece(width)
         piece.set_rotation(PuzzlePiece.Rotation.degree_90)
         # Rotate upper left piece
-        self.assertTrue( piece._get_rotated_coordinates(0,0) == (2,0))
+        self.assertTrue(piece._get_rotated_coordinates(0, 0) == (2, 0))
         # Rotate lower left piece
-        self.assertTrue( piece._get_rotated_coordinates(0,2) == (0,0))
+        self.assertTrue(piece._get_rotated_coordinates(0, 2) == (0, 0))
         # Rotate piece to the right of the top left
-        self.assertTrue( piece._get_rotated_coordinates(1, 0) == (2,1))
+        self.assertTrue(piece._get_rotated_coordinates(1, 0) == (2, 1))
+
+        piece.set_rotation(PuzzlePiece.Rotation.degree_180)
+        # Rotate upper left piece
+        self.assertTrue(piece._get_rotated_coordinates(0, 0) == (2, 2))
+        # Rotate lower left piece
+        self.assertTrue(piece._get_rotated_coordinates(0, 2) == (2, 0))
+        # Rotate piece to the right of the top left
+        self.assertTrue(piece._get_rotated_coordinates(1, 0) == (1, 2))
+
+        # Test a second width
+        width = 9
+        piece = PuzzlePiece(width)
+        for rotation in PuzzlePiece.Rotation.get_all_rotations():
+            piece.set_rotation(rotation)
+            # Since center of the puzzle, rotation should have no effect
+            self.assertTrue(piece._get_rotated_coordinates(4, 4) == (4, 4))
+
+    def test_get_unrotated_puzzle_piece(self):
+        """
+        Tests getting the unrotated x/y coordinate behavior of the puzzle piece
+        module.  It does this test by first performing a rotation, then performing
+        an unrotation and seeing if the unrotated values match the original
+        x/y coordinates.
+        """
+
+        piece = PuzzlePiece(3)
+        piece.set_rotation(PuzzlePiece.Rotation.degree_180)
+        self.assertTrue(piece._get_unrotated_coordinates(2, 2) == (0, 0))
+
+        # Test  a set of widths
+        test_widths = [1, 3, 5, 6, 10, 15]
+        for width in test_widths:
+            # Create an example piece
+            piece = PuzzlePiece(width)
+            # Go through all the rotations
+            for rotation in PuzzlePiece.Rotation.get_all_rotations():
+                piece.set_rotation(rotation)
+                # Test all possible x coordinates
+                for x in range(0, width):
+                    # Test all possible y coordinates
+                    for y in range(0, width):
+                        rotated_x, rotated_y = piece._get_rotated_coordinates(x, y)
+                        unrotated_x, unrotated_y = piece._get_unrotated_coordinates(rotated_x,
+                                                                                    rotated_y)
+                        # Print test conditions if enabled.
+                        if PuzzlePieceTestCase.PRINT_DEBUG_MESSAGES:
+                            print "Setting: width = %d, x = %d, y = %d, rotation = %d" % (width, x, y, rotation.value)
+                            print "rotated_x = %d, rotated_y = %d" % (rotated_x, rotated_y)
+                            print "unrotated_x = %d, unrotated_y = %d" % (unrotated_x, unrotated_y)
+                        # Check the calculated value.
+                        self.assertTrue((x, y) == (unrotated_x, unrotated_y))
+
 
 # If this function is main, then run the unit tests.
 if __name__ == "__main__":
