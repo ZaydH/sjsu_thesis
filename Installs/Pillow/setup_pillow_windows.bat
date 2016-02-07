1babfe71b1fbaa6ff3c768690df64fa7573365b3
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-02-07 03:34:30 -0800

    Uploading a script to install the python libraries on windows.

---
+++ Installs/Pillow/setup_pillow_windows.bat
@@ -0,0 +1,7 @@
+python -m pip install pip --upgrade
+python -m pip install Pillow --upgrade
+python -m pip install enum34 --upgrade
+python -m pip install docutils --upgrade
+
+echo "Packages installed."
+pause
