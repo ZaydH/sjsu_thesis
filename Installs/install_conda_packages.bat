bdd57d32c3e22d1c97a6d9c865ea11323f66ab41
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-03-10 19:45:16 -0800

    Cleaning up the printing in the conda installer.

--- Installs/install_conda_packages.bat
+++ Installs/install_conda_packages.bat
@@ -1,23 +1,37 @@
-echo Updating Python to the latest 2.x version
+@echo off 
+echo.
+echo.
+echo.
+echo.Updating Python to the latest 2.x version
 conda update python
-echo Python update complete.
+echo.Python update complete.
 
-echo 
-echo 
-echo Installing Python packages.
-conda install pip -y
-conda install Pillow -y
-conda install enum34 -y
-conda install docutils -y
-conda install sphinx -y
-conda install numpy -y
-conda install scitkit-image -y
-conda install scipy -y
-conda install scikit-learn -y
+echo. 
+echo.  
+echo.Installing Python packages.
+call:conda_install pip -y
+call:conda_install Pillow -y
+call:conda_install enum34 -y
+call:conda_install docutils -y
+call:conda_install sphinx -y
+call:conda_install numpy -y
+call:conda_install scitkit-image -y
+call:conda_install scipy -y
+call:conda_install scikit-learn -y
 
 
 echo.
 echo.
-echo. End of Package Installation.
+echo.End of Package Installation.
 pause
+goto:eof
+
+
+:conda_install
+echo.
+echo.
+echo.
+echo.Installing package: %~1
+conda install %~1 -y
+echo.
 goto:eof
