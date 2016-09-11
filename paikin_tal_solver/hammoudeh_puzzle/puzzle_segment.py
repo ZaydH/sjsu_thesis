c1eb98242aa5cc682b414877a6ca843381598447
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-09-10 20:40:06 -0700

    Fleshing out the segment class more to support the segmentation needed to improve piece selection.

--- paikin_tal_solver/hammoudeh_puzzle/puzzle_segment.py
+++ paikin_tal_solver/hammoudeh_puzzle/puzzle_segment.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class PuzzleSegment(object):
     """
     This class is used to store a puzzle the information associated with a puzzle segment.
@@ -5,17 +8,19 @@ class PuzzleSegment(object):
 
     _PERFORM_ASSERTION_CHECKS = True
 
-    def __init__(self, puzzle_id, id_number):
+    def __init__(self, puzzle_id, segment_id_number):
         """
         Puzzle segment constructor.
 
         Args:
-            id_number (int): Unique identification number for the segment.
+            puzzle_id (int): Identification number of the solved puzzle
+            segment_id_number (int): Unique identification number for the segment.
         """
         self._puzzle_id = puzzle_id
-        self._id_number = id_number
+        self._segment_id_number = segment_id_number
         self._numb_pieces = 0
         self._piece_ids = {}
+        self._neighbor_segment_ids = {}
 
     @property
     def puzzle_id(self):
@@ -35,7 +40,7 @@ class PuzzleSegment(object):
 
         Returns (int): Segment ID number
         """
-        return self._id_number
+        return self._segment_id_number
 
     @property
     def numb_pieces(self):
@@ -46,15 +51,16 @@ class PuzzleSegment(object):
         """
         return self._numb_pieces
 
-    def add_piece(self, piece_id):
+    def add_piece(self, piece_id, piece_location):
         """
         Adds a puzzle piece (as defined by the piece's identification number) to the puzzle segment.
 
         Args:
             piece_id (int): Identification if the puzzle piece to be added to the segment
+            piece_location (Location): Location of the puzzle piece
         """
         key = PuzzleSegment._get_piece_key(piece_id)
-        self._piece_ids[key] = piece_id
+        self._piece_ids[key] = (piece_id, piece_location)
 
     def remove_piece(self, piece_id):
         """
@@ -62,7 +68,6 @@ class PuzzleSegment(object):
 
         Args:
             piece_id (int): Identification if the puzzle piece to be removed from the segment
-
         """
 
         key = PuzzleSegment._get_piece_key(piece_id)
@@ -71,10 +76,55 @@ class PuzzleSegment(object):
             assert key in self._piece_ids
         del self._piece_ids[key]
 
+    def add_neighboring_segment(self, neighbor_segment_id):
+        """
+        Adds a neighboring segment number for this segment.  Neighboring segment numbers are used to determine the
+        degree of each segment and for coloring the visualization.
+
+        Args:
+            neighbor_segment_id (int): Identification number of the neighboring segment.
+        """
+        segment_key = PuzzleSegment._get_segment_key(neighbor_segment_id)
+
+        # Verify that the neighbor ID does not match its own ID
+        if PuzzleSegment._PERFORM_ASSERTION_CHECKS:
+            assert self._segment_id_number != neighbor_segment_id
+
+        # If neighbor does not exist in the list, add the neighbor to the list
+        if segment_key not in self._neighbor_segment_ids:
+            self._neighbor_segment_ids[segment_key] = neighbor_segment_id
+
+    def get_neighbor_segment_ids(self):
+        """
+        Accessor for the segments neighboring this segment.
+
+        Returns (Dict[int]): The identification numbers of all segments that are adjacent to this segment.
+        """
+        # When getting the neighbor ids in the normal flow, this should not be blank
+        if self._PERFORM_ASSERTION_CHECKS:
+            assert self.neighbor_degree > 0
+
+        return copy.deepcopy(self._neighbor_segment_ids)
+
+    @property
+    def neighbor_degree(self):
+        """
+        Property for getting the number of segments that are adjacent to this puzzle.
+
+        Returns (int): Number of segments that are adjacent to this puzzle.
+        """
+        numb_neighbors = len(self._neighbor_segment_ids)
+
+        # Verify the degree is greater than 0
+        if PuzzleSegment._PERFORM_ASSERTION_CHECKS:
+            assert numb_neighbors > 0
+
+        return numb_neighbors
+
     @staticmethod
     def _get_piece_key(piece_id):
         """
-        Puzzle piece identification numbers are stored in a Python dictinary.  As such, a key is needed to insert
+        Puzzle piece identification numbers are stored in a Python dictionary.  As such, a key is needed to insert
         and remove elements from the dictionary.  This function is used to generate said key.
 
         Args:
@@ -83,3 +133,16 @@ class PuzzleSegment(object):
         Returns (String): Key associated with the puzzle piece that is used by the PuzzleSegment class.
         """
         return str(piece_id)
+
+    @staticmethod
+    def _get_segment_key(segment_id):
+        """
+        Neighboring segment information is stored in a dictionary.  This function is used create that key identification
+        number.
+
+        Args:
+            segment_id (int): Identification number of a segment
+
+        Returns (String): Key associated with the puzzle segment identification number.
+        """
+        return str(segment_id)
