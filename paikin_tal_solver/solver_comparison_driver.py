2dcd72a41d000c9665bc6874ce82b10dcad5dffb
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-10-01 22:40:52 -0700

    Fundamental Bug in Solver Comparison Driver * Going to need a major rewrite.

--- paikin_tal_solver/solver_comparison_driver.py
+++ paikin_tal_solver/solver_comparison_driver.py
@@ -1,11 +1,13 @@
+import logging
 import os
 
 from hammoudeh_puzzle import config
 from hammoudeh_puzzle.puzzle_importer import PuzzleType
-from paikan_tal_solver_driver import paikin_tal_driver
+from multipuzzle_solver_driver import run_multipuzzle_solver_driver
+from paikan_tal_solver_driver import run_paikin_tal_driver
 
-SINGLE_PUZZLE_PROGRESS_FILENAME = ".\\single_puzzle_progress.txt"
-TWO_PUZZLE_PROGRESS_FILENAME = ".\\two_puzzle_progress.txt"
+_PROGRESS_TRACKING_FOLDER = ".\\progress_tracker\\"
+_IMAGE_ID_FILE_SEPARATOR = ","
 
 PUZZLE_TYPE = PuzzleType.type2
 PIECE_WIDTH = 28  # pixels
@@ -15,63 +17,124 @@ def run_comparison_driver():
     _perform_single_puzzle_solving()
 
 
-def _perform_single_puzzle_solving():
+def _perform_805_piece_comparison_puzzle_solving(dataset_name, numb_simultaneous_puzzles, minimum_image_number,
+                                                 maximum_image_number, build_image_filename_function):
     """
-    Runs all of the 20 805 piece images as individual puzzles.  These serve as the baseline for optimal performance.
+    Runs all possible combinations of the Paikin and Tal solver of the specified size.  This uses the 805 piece data
+    set from Ben Gurion University.
+
+    Args:
+        numb_simultaneous_puzzles (int): Number of images to be supplied to the solver.s
     """
+    if numb_simultaneous_puzzles < 1:
+        raise ValueError("The number of simultaneous puzzle to solve must be greater than or equal to 1."
+                         )
+    if maximum_image_number < minimum_image_number:
+        raise ValueError("The maximum image number is less than the minimum image number.")
+
+    if numb_simultaneous_puzzles < maximum_image_number - minimum_image_number + 1:
+        raise ValueError("The number of simultaneous images exceeds the specified size of the dataset.")
+
     # Run single puzzle solver
     while True:
 
-        # If file does not exist, then
-        if not os.path.exists(SINGLE_PUZZLE_PROGRESS_FILENAME):
-            next_puzzle_id = config.MINIMUM_805_PIECE_IMAGE_NUMBER
+        progress_filename = _build_progress_filename(dataset_name, numb_simultaneous_puzzles)
+
+        # If file does not exist, then create the list of images
+        if not os.path.exists(progress_filename):
+            puzzle_id_list = [i + minimum_image_number for i in xrange(0, numb_simultaneous_puzzles)]
         else:
             # Get the next puzzle from the file
-            with open(SINGLE_PUZZLE_PROGRESS_FILENAME, 'r') as progress_file:
-                next_puzzle_id = int(progress_file.readline())
+            with open(progress_filename, 'r') as progress_file:
+                puzzle_id_str_list = progress_file.readline().split(_IMAGE_ID_FILE_SEPARATOR)
+            puzzle_id_list = [int(puzzle_id_str) for puzzle_id_str in puzzle_id_str_list]
 
         # If all images already completed, then exit.
-        if next_puzzle_id > config.MAXIMUM_805_PIECE_IMAGE_NUMBER:
-            break
+        if puzzle_id_list[0] > maximum_image_number - numb_simultaneous_puzzles:
+            return
 
         # Build the image file
-        image_filename = [config.build_805_piece_filename(next_puzzle_id)]
-        paikin_tal_driver(image_filename, PUZZLE_TYPE, PIECE_WIDTH)
+        image_filenames = [build_image_filename_function(puzzle_id) for puzzle_id in puzzle_id_list]
 
-        # Go to the next puzzle and update the progress tracker
-        next_puzzle_id += 1
-        with open(SINGLE_PUZZLE_PROGRESS_FILENAME, 'w') as progress_file:
-            progress_file.write(str(next_puzzle_id))
+        # Run the Multipuzzle Solver.  Paikin Tal will then use MultiPuzzle's pickle export.
+        run_multipuzzle_solver_driver(image_filenames, PUZZLE_TYPE, PIECE_WIDTH)
+        run_paikin_tal_driver(image_filenames, PUZZLE_TYPE, PIECE_WIDTH)
 
-def _perform_two_puzzle_solving():
-
-    # Run single puzzle solver
-    while True:
-
-        # If file does not exist, then
-        if not os.path.exists(TWO_PUZZLE_PROGRESS_FILENAME):
-            first_puzzle_id = config.MINIMUM_805_PIECE_IMAGE_NUMBER
-            second_puzzle_id = first_puzzle_id + 1
-        else:
-            # Get the image numbers for the first and second puzzle
-            with open(SINGLE_PUZZLE_PROGRESS_FILENAME, 'r') as progress_file:
-                split_line = progress_file.readline().split(",")
-            first_puzzle_id = int(split_line[0])
-            second_puzzle_id = int(split_line[1])
-
-        # If all images already completed, then exit.
-        if first_puzzle_id > config.MAXIMUM_805_PIECE_IMAGE_NUMBER:
-            break
+        puzzle_id_list = _increment_puzzle_id_list(puzzle_id_list, maximum_image_number)
+        # Make the tracking file.
+        _write_progress_file(progress_filename, puzzle_id_list)
 
 
-        # Go to the next puzzle and update the progress tracker
-        second_puzzle_id += 1
-        # Handle overflow of second puzzle IDD
-        if second_puzzle_id > config.MAXIMUM_805_PIECE_IMAGE_NUMBER:
-            first_puzzle_id += 1
-            second_puzzle_id += 1
-        with open(SINGLE_PUZZLE_PROGRESS_FILENAME, 'w') as progress_file:
-            progress_file.write(str(first_puzzle_id) + "," str(second_puzzle_id))
+def _build_progress_filename(dataset_name, numb_simultaneous_puzzles):
+    """
+    Helper method to standardize creation of progress file names f
+
+    Args:
+        dataset_name (str): Unique descriptor for solver progress solving.
+        numb_simultaneous_puzzles (int): Number of puzzles to solve simultaneously.
+
+    Returns (str): Filename and path for a standardized naming of this dataset progress tracker.
+    """
+    return _PROGRESS_TRACKING_FOLDER + dataset_name + "_numb_puzzles_" + str(numb_simultaneous_puzzles) + ".csv"
+
+
+def _increment_puzzle_id_list(puzzle_id_list, maximum_puzzle_id_number):
+    """
+    Manages incrementing the list of puzzle identification number to cover all possible puzzle combinations.
+
+    Args:
+        puzzle_id_list (List[int]): Identification numbers of the puzzles being solved.
+        maximum_puzzle_id_number (int): Maximum puzzle ID number for this dataset
+
+    Returns (List[int]): Incremented puzzle IDs.
+    """
+
+    increment_previous = True
+    index_to_increment = len(puzzle_id_list)
+
+    # Increments puzzle identification numbers as necessary
+    while increment_previous:
+        # Go to previous puzzle
+        index_to_increment -= 1
+        increment_previous = False
+
+        # Increment puzzle id
+        puzzle_id_list[index_to_increment] += 1
+
+        # This is needed because only last index actually reaches max index.  Previous ones are offset from that
+        # since we are doing combinations.
+        max_id_offset = len(puzzle_id_list) - index_to_increment - 1
+        # Check for overflow
+        if puzzle_id_list[index_to_increment] > maximum_puzzle_id_number - max_id_offset:
+            if index_to_increment > 0:
+                puzzle_id_list[index_to_increment] = puzzle_id_list[index_to_increment - 1] + 2
+                increment_previous = True              # Prepare for next puzzle getting incremented
+            else:
+                puzzle_id_list[index_to_increment] += 1
+
+    return puzzle_id_list
+
+
+def _write_progress_file(progress_filename, puzzle_id_list):
+    """
+    Writes the puzzle ID list to the specified output file.
+
+    Note: This function takes care of creating the output directory as well.
+
+    Args:
+        progress_filename (str): Name and path of the progress file to be created.
+        puzzle_id_list (List[int]): Identification of the next puzzle IDs to be run through the solver
+    """
+    # Create the
+    progress_file_dir = os.path.dirname(progress_filename)
+    if not os.path.exists(progress_file_dir):
+        logging.info("Creating progress file directory: \"progress_file_dir\"")
+        os.makedirs(progress_filename)
+    with open(progress_filename, 'w') as progress_file:
+        for idx, puzzle_id in enumerate(puzzle_id_list):
+            if idx > 0:
+                progress_file.write(',')
+            progress_file.write(str(puzzle_id))
 
 
 if __name__ == '__main__':
---
+++ paikin_tal_solver/test_cases/comparison_driver_tester.py
@@ -0,0 +1,27 @@
+"""Unittest Module for the Comparison Driver
+
+.. moduleauthor:: Zayd Hammoudeh <hammoudeh@gmail.com>
+"""
+
+import unittest
+
+from solver_comparison_driver import _increment_puzzle_id_list
+
+
+class ComparisonSolverTester(unittest.TestCase):
+
+    def test_puzzle_identification_incrementer(self):
+        """
+        Verify the identification number incrementer works correctly.
+        """
+        self.assertTrue([1, 2, 4] == _increment_puzzle_id_list([1, 2, 3], 20))
+
+        self.assertTrue([1, 2, 20] == _increment_puzzle_id_list([1, 2, 19], 20))
+
+        self.assertTrue([1, 3, 4] == _increment_puzzle_id_list([1, 2, 20], 20))
+
+        self.assertTrue([1, 19, 20] == _increment_puzzle_id_list([1, 18, 20], 20))
+
+        self.assertTrue([2, 3, 4] == _increment_puzzle_id_list([1, 19, 20], 20))
+
+        self.assertTrue([19, 20, 21] == _increment_puzzle_id_list([18, 19, 20], 20))
